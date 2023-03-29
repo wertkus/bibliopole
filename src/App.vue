4b3cbd11b1da155174a46c477defd539867f80fb
Mert <mertkus54@gmail.com>
2023-03-29 14:09:16 +0300

    banner component added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
+  <!--NavBar-->
   <app-header />
 
   <!-- Introduction -->
-  <section class="mb-0 py-20 text-white text-center relative">
-    <div
-      class="absolute inset-0 w-full h-full introduction-bg background-header"
-    ></div>
-    <div class="container mx-auto">
-      <div class="text-white main-header-content">
-        <h1 class="font-serif text-5xl mb-5">Bibliophile (🧙🏼‍♂️/🧙🏼‍♀️)</h1>
-        <p class="w-full md:w-8/12 mx-auto">
-          A bibliophile is someone who loves books and reading. The word
-          "bibliophile" comes from the Greek words "biblion" (book) and "philos"
-          (loving). Bibliophiles are often avid collectors of books and enjoy
-          spending time browsing and reading in bookstores, libraries, or their
-          personal collections ...
-        </p>
-        <p class="font-bold">
-          We Are Here for you, as a bibliophers; trying to serve most rarely
-          books!
-        </p>
-      </div>
-    </div>
-  </section>
+  <main-banner></main-banner>
 
   <!-- Main Content -->
   <product-list></product-list>
@@ -34,6 +15,7 @@
 import AppHeader from "@/components/Header.vue";
 import Auth from "@/components/Auth.vue";
 import ProductList from "@/components/ProductList.vue";
+import MainBanner from "./components/MainBanner.vue";
 
 export default {
   name: "App",
@@ -41,6 +23,7 @@ export default {
     AppHeader,
     Auth,
     ProductList,
+    MainBanner,
   },
 };
 </script>
---
+++ src/components/ProfileBanner.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="mb-0 py-20 text-white text-center relative">
+    <div
+      class="absolute inset-0 w-full h-full introduction-bg background-header"
+    ></div>
+    <div class="container mx-auto">
+      <div class="text-white main-header-content">
+        <h1 class="font-serif text-5xl mb-5">Bibliophile (🧙🏼‍♂️/🧙🏼‍♀️)</h1>
+        <p class="w-full md:w-8/12 mx-auto">
+          A bibliophile is someone who loves books and reading. The word
+          "bibliophile" comes from the Greek words "biblion" (book) and "philos"
+          (loving). Bibliophiles are often avid collectors of books and enjoy
+          spending time browsing and reading in bookstores, libraries, or their
+          personal collections ...
+        </p>
+        <p class="font-bold">
+          We Are Here for you, as a bibliophers; trying to serve most rarely
+          books!
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapStores } from "pinia";
+import useModalStore from "@/stores/modal";
+
+export default {
+  name: "AppHeader",
+  computed: {
+    ...mapStores(useModalStore),
+  },
+  methods: {
+    toggleAuthModal() {
+      this.modalStore.isOpen = !this.modalStore.isOpen;
+      console.log(this.modalStore.isOpen);
+    },
+  },
+};
+</script>
